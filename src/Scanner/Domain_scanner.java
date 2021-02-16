de2e301f2898248d8aa278408ee6cf8994e39c05
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-02-16 03:29:23 +0100

    scan_domain

--- src/Scanner/Domain_scanner.java
+++ src/Scanner/Domain_scanner.java
@@ -5,10 +5,44 @@
  */
 package Scanner;
 
+import java.util.ArrayList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
 /**
  *
  * @author dell
  */
-public class Domain_scanner {
+public class Domain_scanner extends ElementScanner{
     
+    public Domain_scanner(final Document doc){
+        super(doc);
+    }
+    
+    @Override
+    public void scan_info(Element Domain_element){
+        
+        if(Domain_element.getElementsByTagName("usersort").getLength()>0){
+            this.scan_domain_classes(Domain_element);
+        }else{
+            throw new NullPointerException("Can't find domain type: " + this.get_element_name(Domain_element));
+        }
+    }
+    
+    private void scan_domain_classes(Element Domain_element){ //namedsort tag
+        NodeList classes_decl = Domain_element.getElementsByTagName("usersort");
+        ArrayList<String> classes = new ArrayList<>();
+        
+        for(var i = 0; i< classes_decl.getLength(); i++){
+            Node class_decl = classes_decl.item(i);
+            if(class_decl.getNodeType() == Node.ELEMENT_NODE){
+                Element color_class = (Element) class_decl;
+                classes.add(color_class.getAttribute("declaration"));
+            }
+        }
+        //call DataParser function to create Domain of extracted data
+        dp.add_Domain(Domain_element.getAttribute("id"), classes);
+    }
 }
