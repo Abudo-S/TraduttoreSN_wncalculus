a4342a50f3a3424a2207883c74f30c79a30a0a28
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-02-26 22:34:32 +0100

    add_transition_to_struttura_sn

--- src/struttura_sn/Transition.java
+++ src/struttura_sn/Transition.java
@@ -5,10 +5,50 @@
  */
 package struttura_sn;
 
+import java.util.HashMap;
+import wncalculus.guard.Guard;
+
 /**
  *
  * @author dell
  */
 public class Transition extends Node{
     
+    private final Guard guard;
+    
+    public Transition(String name, Guard g){
+        this.name = name;
+        this.Next = new HashMap<>();
+        this.previous = new HashMap<>();
+        this.guard = g;
+    }
+    
+    //next/previous node of a transition is a place
+    //next arc of a transition is a transitioning arc
+    public Place add_next_Node(TArc arc, Place p){
+        this.Next.put(arc, p);
+        p.add_previous_Node(arc, this);
+        return p;
+    }
+    
+    //previous arc of a transition might be an inhibitor or a transitioning arc
+    public void add_previous_Node(Arc arc, Place p){
+        this.previous.put(arc, p);
+    }
+    
+    public Node get_next_by_Arc(TArc arc){
+        return this.Next.get(arc);
+    }
+    
+    public Node get_previous_by_Arc(Arc arc){
+        return this.previous.get(arc);
+    }
+    
+    public Guard get_guard(){
+        return this.guard;
+    }
+    
+    /*public boolean is_enabled(){
+        return false;
+    }*/
 }
