b05d585bba9b9fe851bc7a63e5b05ac00a363d4a
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-02-26 22:34:16 +0100

    add_token_to_struttura_sn

--- src/struttura_sn/Token.java
+++ src/struttura_sn/Token.java
@@ -5,10 +5,53 @@
  */
 package struttura_sn;
 
+import wncalculus.color.ColorClass;
+
 /**
  *
  * @author dell
  */
 public class Token {
     
+    private final String value; //in case of domain token then it will be value1,value2,...
+    private final ColorClass class_type ;
+    private Place current_place;
+   
+    //can't be token of colour domain and colour class at the same time
+    public Token(String value, ColorClass type){
+        this.value = value;
+        this.class_type = type;
+    }
+    
+    public void set_current_place(Place p){
+        this.current_place = p;
+    }
+    
+    public ColorClass get_type(){
+        return this.class_type;
+    }
+    
+    public String get_Token_value(){
+        return this.value;
+    }
+    
+    public Place get_current_place(){
+        return this.current_place;
+    }
+    
+    /*
+    public void set_type(ColourClass type) throws Exception{
+        if(this.domain_type != null){
+            throw new Exception("Type conflict: Trying to assign same token to different types");
+        }
+        this.class_type = type;
+    }
+    
+    public void set_type(Domain type) throws Exception{
+        if(this.class_type != null){
+            throw new Exception("Type conflict: Trying to assign same token to different types");
+        }
+         this.domain_type = type;
+    }
+    */
 }
