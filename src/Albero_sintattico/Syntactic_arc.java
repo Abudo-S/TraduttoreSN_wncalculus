2e2225590b4a1eff20c75b6617e197570adbec3f
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-11 00:27:57 +0100

    add_Syntactic_arc

--- src/Albero_sintattico/Syntactic_arc.java
+++ src/Albero_sintattico/Syntactic_arc.java
@@ -5,14 +5,28 @@
  */
 package Albero_sintattico;
 
+import java.util.HashMap;
 /**
  *
  * @author dell
  */
 public class Syntactic_arc extends SyntacticElement{
     
+    private HashMap<Syntactic_tuple, Integer> multiplied_tuples;
+    
     public Syntactic_arc(String name){
         super(name);
     }
     
+    public void add_multiplied_tuple(Syntactic_tuple st, int multiplicity){
+        this.multiplied_tuples.put(st, multiplicity);
+    }
+    
+    public HashMap<Syntactic_tuple, Integer> get_all_tuples(){
+        return this.multiplied_tuples;
+    }
+    
+    public int get_tuple_multiplicity(Syntactic_tuple st){
+        return this.multiplied_tuples.get(st);
+    }
 }
