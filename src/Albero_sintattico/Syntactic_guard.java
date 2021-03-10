18a474c126e8ac8dfb6ab743ae09d2820cbb0e4a
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-11 00:28:29 +0100

    add_Syntactic_guard

--- src/Albero_sintattico/Syntactic_guard.java
+++ src/Albero_sintattico/Syntactic_guard.java
@@ -5,10 +5,27 @@
  */
 package Albero_sintattico;
 
+import java.util.LinkedHashMap;
+
 /**
  *
  * @author dell
  */
-public class Syntactic_guard { //could be element of Syntactic_arc or Syntactic_transition
+public class Syntactic_guard { //could be element of Syntactic_tuple or Syntactic_transition
     
+    private final boolean invert_guard;
+    private final LinkedHashMap<Syntactic_predicate, String> separated_predicates;
+    
+    public Syntactic_guard(boolean invert_guard, LinkedHashMap<Syntactic_predicate, String> separated_predicates){
+        this.invert_guard = invert_guard;
+        this.separated_predicates = separated_predicates;
+    }
+    
+    public boolean get_invert_guard(){
+        return this.invert_guard;
+    }
+    
+    public LinkedHashMap<Syntactic_predicate, String> get_separated_predicates(){
+        return this.separated_predicates;
+    }
 }
