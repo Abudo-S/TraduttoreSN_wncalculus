51dd87b7ab2a113b9408708c267aa40b96e55f23
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-22 15:27:48 +0100

    create-Semantic_DataTester

---
+++ src/Test/Semantic_DataTester.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import Albero_sintattico.Syntactic_predicate;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import wncalculus.expr.Domain;
+import wncalculus.guard.Guard;
+
+/**
+ *
+ * @author dell
+ */
+//singleton
+public class Semantic_DataTester { //for guards and tuples 
+    //single instance
+    private static Semantic_DataTester instance = null;
+    
+    private Semantic_DataTester(){
+        
+    }
+    
+    public void test_semantic_guard(LinkedHashMap<Syntactic_predicate, String> separated_predicates, Guard g){
+        //to be completed
+    }
+    
+    public void test_semantic_predicate(Syntactic_predicate synt_pr, Guard g){
+       //to be completed
+    }
+    
+    public void test_semantic_arc(){
+        //to be completed
+    }
+    
+    public void test_semantic_arc_tuple(){
+        //to be completed
+    }
+    
+    public void test_domain(String element_name, Domain d){ //for WNtuple/transition objects
+        //to be completed
+    }
+    
+    public static Semantic_DataTester get_instance(){
+        
+        if(instance == null){
+            instance = new Semantic_DataTester();
+        }
+        
+        return instance;
+    }
+}
