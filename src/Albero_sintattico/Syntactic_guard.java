91d9a46d04f384b1ac1964ad0f7fec12ef631f2e
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-26 00:41:41 +0100

    add-javadoc_comments_to_Syntactic_guard

--- src/Albero_sintattico/Syntactic_guard.java
+++ src/Albero_sintattico/Syntactic_guard.java
@@ -16,15 +16,28 @@ public class Syntactic_guard { //could be element of Syntactic_tuple or Syntacti
     private final boolean invert_guard;
     private final LinkedHashMap<Syntactic_predicate, String> separated_predicates;
     
+    /**
+     * 
+     * @param invert_guard flag that signs if this guard inverted with (!-NOT)
+     * @param separated_predicates map of syntactic predicates that each syntactic predicate is associated with a separator sign (and/or) with next syntactic predicate if exists
+     */
     public Syntactic_guard(boolean invert_guard, LinkedHashMap<Syntactic_predicate, String> separated_predicates){
         this.invert_guard = invert_guard;
         this.separated_predicates = separated_predicates;
     }
     
+    /**
+     * 
+     * @return if guard is inverted
+     */
     public boolean get_invert_guard(){
         return this.invert_guard;
     }
     
+    /**
+     * 
+     * @return map of syntactic predicates that each syntactic predicate is associated with a separator sign (and/or) with next syntactic predicate if exists
+     */
     public LinkedHashMap<Syntactic_predicate, String> get_separated_predicates(){
         return this.separated_predicates;
     }
