cc0a69e8146bb8f899a80f9ee5e3481e537d8a37
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-26 00:43:07 +0100

    add-javadoc_comments_to_Place_syntax_table

--- src/Componenti/Place_syntax_table.java
+++ src/Componenti/Place_syntax_table.java
@@ -25,18 +25,39 @@ public class Place_syntax_table { //used for analyzing "All" class functuon to f
         this.type_values = new HashMap<>();
     }
     
+    /**
+     * 
+     * @param place_name place name in SN
+     * @param type place type (colour class/domain)
+     */
     public void add_place_type(String place_name, String type){
         this.syntax.put(place_name, type);
     }
     
+    /**
+     * 
+     * @param type a place type (colour class/domain)
+     * @param value ArrayList of ordered values of that type
+     * Note: if type is a colour class then the corresponding ArrayList will contain only one element which is colour-class's name
+     * Note: if type is a domain then the corresponding ArrayList will contain n > 1 elements which are colour-classes names
+     */
     public void add_type_value(String type, ArrayList<String> value){
         this.type_values.put(type, value);
     }
     
+    /**
+     * 
+     * @param place_name place name that we want to know its values that should follow a certain colour-classes ordering/syntax
+     * @return place's colour-classes ordering/syntax
+     */
     public ArrayList<String> get_place_values(String place_name){
         return this.type_values.get(this.syntax.get(place_name));
     }
     
+    /**
+     * 
+     * @return single static instance
+     */
     public static Place_syntax_table get_instance(){
 
         if(instance == null){
