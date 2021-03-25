a677216cd381f93be3d36488fb48837f53e32365
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-26 00:42:20 +0100

    add-javadoc_comments_to_Syntactic_tuple

--- src/Albero_sintattico/Syntactic_tuple.java
+++ src/Albero_sintattico/Syntactic_tuple.java
@@ -14,18 +14,34 @@ public class Syntactic_tuple { //element of Syntactic_arc
     private Syntactic_guard sg;
     private final String[] tuple_elements;
     
+    /**
+     * 
+     * @param tuple_elements Array of all elements of tuple
+     */
     public Syntactic_tuple(String[] tuple_elements){
         this.tuple_elements = tuple_elements;
     }
     
+    /**
+     * 
+     * @param sg syntactic guard of this tuple object
+     */
     public void set_syntactic_guard(Syntactic_guard sg){
         this.sg = sg;
     }
     
+    /**
+     * 
+     * @return syntactic guard of this tuple object
+     */
     public Syntactic_guard get_syntactic_guard(){
         return this.sg;
     }
     
+    /**
+     * 
+     * @return Array of all elements of tuple
+     */
     public String[] get_tuple_elements(){
         return this.tuple_elements;
     }
