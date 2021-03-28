909f850f62011c5ff38f88174a698f18a5e3dbcd
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-28 15:06:42 +0200

    add-javadoc_comments_to_Variable

--- src/struttura_sn/Variable.java
+++ src/struttura_sn/Variable.java
@@ -22,20 +22,38 @@ public class Variable { //a projection is a variable in arc expression
     private HashMap<Transition, ArrayList<Projection>> available_projections_t; //all projections of variable that are written on transition, will be empty while variable declaration
     //Note: ArrayList<Projection> of a transition may contains x, x++, x--
     
+    /**
+     * 
+     * @param variable_name the name of variable
+     * @param colour_type the colour-class type of variable
+     */
     public Variable(String variable_name, ColorClass colour_type){
         this.variable_name = variable_name;
         this.colour_type = colour_type;
         this.available_projections_t = new HashMap<>();
     }
     
+    /**
+     * 
+     * @return the name of variable
+     */
     public String get_name(){
         return this.variable_name;
     }
     
+    /**
+     * 
+     * @return the colour-class type of variable
+     */
     public ColorClass get_colourClass(){
         return this.colour_type;
     }
     
+    /**
+     * 
+     * @param p an available created projection that refers to this variable
+     * @param transition_name the name of transition associated with projection (p)
+     */
     public void add_available_projection(Projection p, String transition_name){
         this.available_projections_t.keySet().stream().filter(
                 t -> t.get_name().equals(transition_name)
@@ -48,6 +66,12 @@ public class Variable { //a projection is a variable in arc expression
         );
     }
     
+    /**
+     * 
+     * @param index the index that we want to check if exists in available projections
+     * @param transition_name the name of transition associated with projection's index
+     * @return true if index is found, false otherwise
+     */
     public boolean check_if_index_exists(int index, String transition_name){    
         ArrayList<Projection> projs;
         //System.out.println(available_projections_t.size());
@@ -68,7 +92,14 @@ public class Variable { //a projection is a variable in arc expression
         return false;
     }
     
-    //successor_flag = 1 in case of ++, -1 in case of --, 0 otherwise
+    /**
+     * 
+     * @param index the index that we want to check if exists in available projections
+     * @param transition_name the name of transition associated with projection's index
+     * @param cc the colour class associated with index and successor_flag, from them we create a new projection if isn't available in available_projections_t
+     * @param successor_flag 1 in case of ++, -1 in case of --, 0 otherwise
+     * @return 
+     */
     public Projection get_available_projection(int index, String transition_name, ColorClass cc, int successor_flag){
         Projection[] p_wrapper = new Projection[1];
         
