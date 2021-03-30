4e6028276222d2ba2d87788213974d71bbc12c09
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-03-30 16:43:44 +0200

    add-javadoc_comments_to_ColorClass_tokens_table

--- src/componenti/ColorClass_tokens_table.java
+++ src/componenti/ColorClass_tokens_table.java
@@ -24,34 +24,62 @@ public class ColorClass_tokens_table { //contains each colorclass available toke
     //single instance
     private static ColorClass_tokens_table instance = null; 
     
+    /**
+     * 
+     */
     private ColorClass_tokens_table(){
         this.cc_subccs = new HashMap<>();
         this.subccs_values = new HashMap<>();
         this.subccs_values_exist = new HashMap<>();
     }
     
+    /**
+     * 
+     * @param cc parent colour-class that we want to add its subclasses 
+     * @param subccs ArrayList of of subclasses (case of partitioned colour class) or parent colour-class name (case of non partitioned colour-class)
+     */
     public void add_colorclass_values(String cc, ArrayList<String> subccs){
         this.cc_subccs.put(cc, subccs);
     }
     
+    /**
+     * 
+     * @param cc sub-colour-class / colour-class that we want to add its flag
+     * @param explicit true means that its values/tokens exist (explicit token-expressing), false means that it will be estimated by Token_estimator (implicit token-expressing)
+     */
     public void set_explicit_cc_flag(String cc, boolean explicit){
         this.subccs_values_exist.put(cc, explicit);
     }
     
+    /**
+     * 
+     * @param cc sub-colour-class / colour-class that we want to add its tokens values
+     * @param values ArrayList of tokens values
+     */
     public void add_cc_tokens_values(String cc, ArrayList<String> values){
         this.subccs_values.put(cc, values);
     }
     
-    public ArrayList<String> get_subcc_values(String subcc){
+    /**
+     * 
+     * @param cc sub-colour-class / colour-class that we want know its tokens values
+     * @return ArrayList of explicit tokens if exist (case of tags "finiteenumeration" & "useroperator"), null otherwise (case of tag "finiteintrange")
+     */
+    public ArrayList<String> get_subcc_values(String cc){
         
-        if(!subccs_values_exist.containsKey(subcc) || !this.is_cc_explicit(subcc)){ //assume that subclass isn't explicitly expressed if its keys doesn't exist in "subccs_values_exist"
+        if(!subccs_values_exist.containsKey(cc) || !this.is_cc_explicit(cc)){ //assume that subclass isn't explicitly expressed if its keys doesn't exist in "subccs_values_exist"
             return null;
         }else{ //explicitly expressed subclass
-            return this.subccs_values.get(subcc);
+            return this.subccs_values.get(cc);
         }
 
     }
     
+    /**
+     * 
+     * @param cc the name of colour class/sub-colour-class that we want to know if its tokens are explicitly expressed
+     * @return true if cc is explicitly expressed, false otherwise
+     */
     private boolean is_cc_explicit(String cc){
         return this.subccs_values_exist.get(cc);
     }
