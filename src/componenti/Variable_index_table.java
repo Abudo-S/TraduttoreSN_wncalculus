e982e7d7506011eefa74975e44900eaeeaa60b9b
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-04-13 15:10:13 +0200

    updat1_Variable_index_table

--- src/componenti/Variable_index_table.java
+++ src/componenti/Variable_index_table.java
@@ -5,62 +5,115 @@
  */
 package componenti;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import struttura_sn.SN;
+import struttura_sn.Transition;
+import struttura_sn.Variable;
+import test.Semantic_DataTester;
+import wncalculus.expr.Domain;
 
 /**
  *
  * @author dell
  */
 //singleton
-public class Variable_index_table { //used for assigning projections' indices for each colorclass
-    
-    private HashMap<String, ArrayList<String>> cc_vars_names; //all variables that are created for a colorclass, Note: variable index will be considered as projection index +1
+public class Variable_index_table { //used for assigning projections' indices for each transition 
+    //HashMap<t_name, HashMap<cc_name, HashMap<var_name, int>>> -> HashMap of all reserved indices of a certain colour-class variables for a transition
+    private HashMap<String, HashMap<String, HashMap<String, Integer>>> t_reserved_cc_var_indices; //all reserved indices of a certain transition variables of the same colorclass
     //single instance
     private static Variable_index_table instance = null;
     
     private Variable_index_table(){
-        this.cc_vars_names = new HashMap<>();
+        this.t_reserved_cc_var_indices = new HashMap<>();
     }
     
     /**
      * 
-     * @param cc colour-class name
-     * @param var variable name that is created from cc type
+     * @param tran the name of transition that we want to add its variables that remain around/in it 
+     * @param d the domain of transition that we will extract used colour-classes in it
      */
-    public void add_cc_var_name(String cc, String var){
+    public void initialize_transition_cc(String tran, Domain d){
+        HashMap<String, HashMap<String, Integer>> cc_t_vars = new HashMap<>();
         
-        if(this.cc_vars_names.containsKey(cc)){ //if cc exists then update it's ArrayList of variables
-            ArrayList<String> vars_names = this.cc_vars_names.get(cc);
-            vars_names.add(var);
-            this.cc_vars_names.put(cc, vars_names);
-        }else{ //if cc doesn't exist then add new row
-            this.cc_vars_names.put(cc, new ArrayList<String>(List.of(var)));
-        }
+        d.asMap().keySet().stream().forEach(
+                cc -> cc_t_vars.put(cc.name(), new HashMap<>())
+        );
+        
+        this.t_reserved_cc_var_indices.put(tran, cc_t_vars);
+    }                
+    
+    /**
+     * 
+     * @return HashMap of all reserved indices of a certain transition variables of the same colour-class
+     */
+    public HashMap<String, HashMap<String, HashMap<String, Integer>>> get_all_reserved_indices(){
+        return this.t_reserved_cc_var_indices;
     }
     
     /**
      * 
+     * @param tran the transition that we want to search in its variables
      * @param var variable name that we want to get its index to create a projection
-     * @return the index of variable in the ArrayList (+1) 
+     * @param d the domain of transition (used before transition's creation)
+     * @return the index available
+     * @throws NullPointerException if variable or transition isn't found
+     * @throws RuntimeException if this method can't assign an index
      * Note: +1 is added because that library wncalculus doesn't allow creating a projection with index 0 
+     * Note: wncalculus doesn't allow creating projection index out of its colour-class's multiplicity in transition domain
      */
-    public int get_variable_index(String var){
-        
-        for(String cc : this.cc_vars_names.keySet()){
-            ArrayList<String> cc_vars = this.cc_vars_names.get(cc);
+    public int get_variable_index(String tran, String var, Domain d) throws NullPointerException, RuntimeException{
+        SN sn = SN.get_instance();        
+        Variable v = sn.find_variable(var);   
 
-            for(var i = 0; i < cc_vars.size(); i++){
+        if(v != null){
+            int max_index;
+            Transition t = sn.find_transition(tran);
+            
+            if(t == null){
+                max_index = d.mult(v.get_colourClass());
+            }else{
+                max_index = t.get_node_domain().mult(v.get_colourClass());
+            }
 
-                if(cc_vars.get(i).equals(var)){
-                    return i+1;
+            //Semantic_DataTester.get_instance().test_domain(tran, d);
+            for(int i = 1; i <= max_index; i++){
+                String cc_name = v.get_colourClass().name();
+                HashMap<String, HashMap<String, Integer>> cc_t_vars = this.t_reserved_cc_var_indices.get(tran);
+                HashMap<String, Integer> vars_indices = cc_t_vars.get(cc_name);
+                
+                if(vars_indices.containsKey(var)){ //index is already reserved 
+                    return vars_indices.get(var);
+                    
+                }else{ //new index should be reserved
+                    boolean found = true;
+                    
+                    for(String var_name : vars_indices.keySet()){
+                        
+                        if(vars_indices.get(var_name) == i){ //index is reserved
+                            found = false;
+                        }
+                    }
+                    
+                    if(found){
+                        //update t_reserved_cc_var_indices
+                        vars_indices.put(var, i);
+                        cc_t_vars.put(cc_name, vars_indices);
+                        this.t_reserved_cc_var_indices.put(tran, cc_t_vars);
+                        
+                        return i;
+                    }
                 }
             }
-                    
+            String cc_name = v.get_colourClass().name();
+                HashMap<String, HashMap<String, Integer>> cc_t_vars = this.t_reserved_cc_var_indices.get(tran);
+                HashMap<String, Integer> vars_indices = cc_t_vars.get(cc_name);
+                vars_indices.keySet().stream().forEach(
+                        vr -> System.out.println(vr + "," + vars_indices.get(vr) + "," + tran)
+                );
+            throw new RuntimeException("Can't assign an index to : " + var + " around/in " + tran);
         }
         
-        throw new RuntimeException("Can't find the index of var: " + var);
+        throw new NullPointerException("Can't find variable: " + var);
     }
             
     //successor_flag = 1 in case of ++, -1 in case of --, 0 otherwise
