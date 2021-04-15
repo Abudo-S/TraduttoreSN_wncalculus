dd3cc8870a2a78d8662e367e72b3d9c4557eca8d
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-04-15 13:48:33 +0200

    update-Place_writer/implement-write_info

--- src/writer/Place_writer.java
+++ src/writer/Place_writer.java
@@ -5,7 +5,10 @@
  */
 package writer;
 
+import eccezioni.UnsupportedELementdataException;
+import java.util.ArrayList;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  *
@@ -13,13 +16,47 @@ import org.w3c.dom.Document;
  */
 //singleton
 public class Place_writer extends ElementWriter{
-        //single instance
+    //single instance
     private static Place_writer instance = null;
     
     private Place_writer(Document doc){
         super(doc);
     }
-        
+
+    /**
+     * 
+     * @param element_info ArrayList of element's data that will be added to pnml document
+     * @throws UnsupportedELementdataException if one of element_info internal data can't be transformed in pnml format
+     */
+    @Override
+    public void write_info(ArrayList<String> element_info) throws UnsupportedELementdataException{
+        Element place = doc.createElement("place");
+        doc.appendChild(place);
+                
+        element_info.stream().forEach(
+                single_datum -> {
+                    
+                    switch(single_datum){
+                        case "id": //name
+                            Element name = doc.createElement(single_datum);
+                            name.appendChild(doc.createTextNode(this.seperate_usable_value(single_datum)));
+                            place.setAttribute("id", single_datum);
+                            break;
+                        case "type":
+                            Element type = doc.createElement(single_datum);
+                            type.appendChild(doc.createTextNode(this.seperate_usable_value(single_datum)));
+                            break;
+                        case "hlinitialMarking": //might use Marking_writer
+                            Element hlinitialMarking = doc.createElement(single_datum);
+                            hlinitialMarking.appendChild(doc.createTextNode(this.seperate_usable_value(single_datum)));
+                            break;                            
+                        default:
+                            throw new UnsupportedELementdataException("Can't transform one of element data in pnml: " + single_datum);
+                    }
+                }
+        );
+    }
+    
     /**
      * 
      * @return single static instance
