7be9683ee3366a8fb46b86316f0a6c3978e94e3a
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-04-15 13:48:46 +0200

    update-Transition_writer/implement-write_info

--- src/writer/Transition_writer.java
+++ src/writer/Transition_writer.java
@@ -5,7 +5,11 @@
  */
 package writer;
 
+import eccezioni.UnsupportedELementdataException;
+import java.util.ArrayList;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import static writer.ElementWriter.doc;
 
 /**
  *
@@ -19,7 +23,32 @@ public class Transition_writer extends ElementWriter{
     private Transition_writer(Document doc){
         super(doc);
     }
-        
+    
+    /**
+     * 
+     * @param element_info ArrayList of element's data that will be added to pnml document
+     * @throws UnsupportedELementdataException if one of element_info internal data can't be transformed in pnml format
+     */
+    @Override
+    public void write_info(ArrayList<String> element_info) throws UnsupportedELementdataException{
+        Element transition = doc.createElement("place");
+        doc.appendChild(transition);
+                
+        element_info.stream().forEach(single_datum -> {
+                    
+                    switch(single_datum){
+                        case "id": //name
+                            Element name = doc.createElement(single_datum);
+                            name.appendChild(doc.createTextNode(this.seperate_usable_value(single_datum)));
+                            transition.setAttribute("id", single_datum);
+                            break;                           
+                        default:
+                            throw new UnsupportedELementdataException("Can't transform one of element data in pnml: " + single_datum);
+                    }
+                }
+        );
+    }
+    
     /**
      * 
      * @return single static instance
