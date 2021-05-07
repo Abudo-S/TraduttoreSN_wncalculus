417bbfce1c8b0a1f5ca9812f69607d4af328b1dc
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-05-07 05:45:00 +0200

    update-Arc_writer/add-write_info_pnpro

--- src/writer/Arc_writer.java
+++ src/writer/Arc_writer.java
@@ -7,6 +7,7 @@ package writer;
 
 import eccezioni.UnsupportedElementdataException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import static writer.ElementWriter.doc;
@@ -61,6 +62,35 @@ public class Arc_writer extends ElementWriter{
                         throw new UnsupportedElementdataException("Can't transform one of element data in pnml: " + single_datum);
                     }
                 }
+        );
+        parent.appendChild(arc);
+    }
+    
+    /**
+     * 
+     * @param element_info ArrayList of element's data that will be added to pnpro document
+     * @param parent the element to which data will be added
+     * @throws UnsupportedElementdataException if one of element_info internal data can't be transformed in pnpro format
+     */
+    @Override
+    public void write_info_pnpro(ArrayList<String> element_info, Element parent) throws UnsupportedElementdataException{
+        Element arc = doc_pnpro.createElement("arc");
+
+        element_info.stream().forEach(
+            single_datum -> {
+
+                if(single_datum.contains("head@=")){
+                    arc.setAttribute("head", this.seperate_usable_value(single_datum));
+                }else if(single_datum.contains("tail@=")){
+                    arc.setAttribute("tail", this.seperate_usable_value(single_datum));
+                }else if(single_datum.contains("kind@=")){ //INPUT | OUTPUT | INHIBITOR
+                    arc.setAttribute("kind", this.seperate_usable_value(single_datum));
+                }else if(single_datum.contains("mult@=")){
+                    arc.setAttribute("mult", this.seperate_usable_value(single_datum));
+                }else{
+                    throw new UnsupportedElementdataException("Can't transform one of element data in pnml: " + single_datum);
+                }
+            }
         );
         parent.appendChild(arc);
     }
