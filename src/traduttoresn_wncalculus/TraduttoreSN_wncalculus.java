96112352019ee3ac3063769d9ee1a27b338b4e36
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-05-18 17:17:20 +0200

    update-TraduttoreSN_wncalculus/add-write_original_SN

--- src/traduttoresn_wncalculus/TraduttoreSN_wncalculus.java
+++ src/traduttoresn_wncalculus/TraduttoreSN_wncalculus.java
@@ -10,13 +10,17 @@ import fasi_traduzione.*;
 import eccezioni.UnsupportedFileException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import test.SN_DataTester;
 import test.SyntaxTree_DataTester;
 import java.util.Scanner;
+import javax.xml.parsers.ParserConfigurationException;
+import struttura_sn.Marking;
 import struttura_sn.SN;
 import struttura_sn.Token;
 import test.Semantic_DataTester;
 import wncalculus.expr.Interval;
+import wncalculus.wnbag.LinearComb;
 
 /**
  *
@@ -48,7 +52,9 @@ public class TraduttoreSN_wncalculus {
         //apply unfolding algorithm
         System.out.println("----------------");
         PartialGenerator pg = PartialGenerator.get_instance();
-        pg.unfold_all_places();        
+        pg.unfold_all_places(); 
+        //write original SN
+        write_original_SN(pg);
     }
     
     /**
@@ -105,6 +111,48 @@ public class TraduttoreSN_wncalculus {
         return file_name;
     }
     
+    /**
+     * write all SN original components as a gspn in PNPRO file
+     * @param pg partial generator object that will be used to write original SN components
+     */
+    private static void write_original_SN(PartialGenerator pg) throws Exception{
+        SN sn = SN.get_instance();
+        Marking mk = Marking.get_instance();
+        int[] place_xy = new int[]{40, 20}; //graphics points
+        int[] y_multiplier = new int[]{1};
+        XMLWriter xmlwriter = XMLWriter.get_instance("original");
+        //reset places pnpro list
+        xmlwriter.reset_places_data_pnpro();
+        //reset arcs pnpro list
+        xmlwriter.reset_arcs_data_pnpro();
+        
+        sn.get_P().stream().forEach(
+            place -> {
+                //write place
+                ArrayList<String> place_data = new ArrayList<>();
+                HashMap<ArrayList<LinearComb>, Integer> initial_marking = mk.get_marking_of_place(place.get_name());
+                //copy intitial marking of place in all unfolded places
+                String marking;
+                place_data.add("name@=" + place.get_name());
+                place_data.add("domain@=" + place.get_type());
+
+                if(!initial_marking.isEmpty()){
+                   marking = pg.get_place_initial_marking(initial_marking);
+                   place_data.add("marking@=" + marking);
+                }
+                place_xy[1] += 20;
+                place_data.add("graphics@=" + "x=" + place_xy[0] + "y=" + String.valueOf(place_xy[1] - y_multiplier[0] * 10));
+                y_multiplier[0]++;
+                //add place to be written in pnpro
+                xmlwriter.add_place(place_data, true);
+
+                //write place arcs with unfolded filter
+                pg.write_unfolded_place_arcs(place, "", "");
+            }            
+        );
+        xmlwriter.write_all_data_pnpro(true);
+        
+    }
     /**
      * 
      * @param cc_subcc_name colour class/sub colour-class that we want to estimates its tokens
